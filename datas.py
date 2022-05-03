df75c6567eb01de168f09e64c439d746809e1a0e
김준호 <kesu9251@naver.com>
2022-05-04 00:09:20 +0900

    Data 추가

--- datas.py
+++ datas.py
@@ -45,19 +45,22 @@ class Data:
             super().__setattr__('value', self.demand/self.supply)
 
 #경재요소
-Data(name='소득'), Data(name='자산'), Data(name='부동산')
+Data(name='소득'), Data(name='자산'), Data(name='부동산'), Data(name='한국금리'), Data(name='미국금리'), Data(name='한국채권'), Data(name='미국채권')
 
 #식료품
-Data(name='닭고기'), Data(name='돼지고기'), Data(name='소고기'), Data(name='양고기'), Data(name='오리고기'), Data(name='')
+Data(name='닭고기'), Data(name='돼지고기'), Data(name='소고기'), Data(name='양고기'), Data(name='오리고기')
 
 #전자제품
 Data(name='반도체'), Data(name=''), Data(name=''), Data(name='')
 
 #농업
-Data(name='소득'), Data(name=''), Data(name='')
+Data(name='옥수수'), Data(name='대두'), Data(name='밀'), Data(name='커피'), Data(name='콩')
+Data(name='쌀')
 
-
-#
+#원자재
+Data(name='니켈'), Data(name='금'), Data(name='은')
+Data(name='원유'), Data(name='천연가스')
+Data(name='비료'), Data(name='사료')
 
 if __name__ == '__main__':
     stock = Data(name='주식')
